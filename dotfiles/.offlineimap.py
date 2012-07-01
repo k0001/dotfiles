9274a3a74dc2aa0eba6ad28780bb4248913cae5b
Renzo Carbonara <gnuk0001@gmail.com>
2012-07-01 17:20:36 -0300

    mmm

---
+++ dotfiles/.offlineimap.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+import re
+
+mapping = { 'INBOX': 'INBOX'
+          , '[Gmail]/All Mail': 'all_mail'
+          , '[Gmail]/Drafts': 'drafts'
+          , '[Gmail]/Important': 'important'
+          , '[Gmail]/Sent Mail': 'sent_mail'
+          , '[Gmail]/Spam': 'spam'
+          , '[Gmail]/Starred': 'starred'
+          , '[Gmail]/Trash': 'trash'
+          }
+
+r_mapping = { val: key for key, val in mapping.items() }
+
+def nt_remote(folder):
+    try:
+        return mapping[folder]
+    except:
+        return re.sub(' ', '_', folder).lower()
+
+def nt_local(folder):
+    try:
+        return r_mapping[folder]
+    except:
+        return re.sub('_', ' ', folder).capitalize()
+
+# folderfilter = exclude(['Label', 'Label', ... ])
+def exclude(excludes):
+    def inner(folder):
+        return not folder in excludes
+    return inner
+
+
+
+import sys
+import gtk
+import gnomekeyring as gkey
+
+class Keyring(object):
+    def __init__(self, name, server, protocol):
+        self._name = name
+        self._server = server
+        self._protocol = protocol
+        self._keyring = gkey.get_default_keyring_sync()
+
+    def has_credentials(self):
+        try:
+            attrs = {"server": self._server, "protocol": self._protocol}
+            items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
+            return len(items) > 0
+        except gkey.DeniedError:
+            return False
+
+    def get_credentials(self):
+        attrs = {"server": self._server, "protocol": self._protocol}
+        items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
+        return (items[0].attributes["user"], items[0].secret)
+
+    def set_credentials(self, (user, pw)):
+        attrs = {
+                "user": user,
+                "server": self._server,
+                "protocol": self._protocol,
+            }
+        gkey.item_create_sync(gkey.get_default_keyring_sync(),
+                gkey.ITEM_NETWORK_PASSWORD, self._name, attrs, pw, True)
+
+def get_username(server):
+    keyring = Keyring("offlineimap", server, "imap")
+    (username, password) = keyring.get_credentials()
+    return username
+
+def get_password(server):
+    keyring = Keyring("offlineimap", server, "imap")
+    (username, password) = keyring.get_credentials()
+    return password
+
